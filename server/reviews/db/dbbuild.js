c4c538f9211a9827fcc8c65029cc8e7fff5be0c4
Jason Matta <jt.matta@gmail.com>
2022-06-08 12:35:01 -0700

    add functioning dbbuild script with etl

--- server/reviews/db/dbbuild.js
+++ server/reviews/db/dbbuild.js
@@ -15,10 +15,10 @@ const { dropdbAsync, createdbAsync} = Promise.promisifyAll(pgtools, {multiArgs: 
 dropdbAsync(config, process.env.PGDATABASE)
   .then(() => createdbAsync(config, process.env.PGDATABASE))
   .then(() => db.queryAsync(`CREATE TABLE IF NOT EXISTS Reviews (
-    review_id BIGSERIAL,
-    product_id INTEGER,
+    review_id BIGSERIAL PRIMARY KEY,
+    product_id BIGSERIAL NOT NULL,
     rating INTEGER,
-    date INTEGER,
+    date BIGSERIAL,
     summary TEXT,
     body TEXT,
     recommend BOOLEAN,
@@ -28,23 +28,23 @@ dropdbAsync(config, process.env.PGDATABASE)
     response TEXT,
     helpfulness INTEGER
   )`))
+  .then(() => db.queryAsync(`COPY Reviews FROM '${process.env.REVIEWSDATA}' WITH (FORMAT csv, header)`))
   .then(() => db.queryAsync(`CREATE TABLE IF NOT EXISTS Photos (
-    photo_id BIGSERIAL,
-    review_id BIGSERIAL,
+    photo_id BIGSERIAL PRIMARY KEY,
+    review_id BIGSERIAL NOT NULL,
     url VARCHAR,
     CONSTRAINT fk_review
       FOREIGN KEY(review_id)
 	      REFERENCES Reviews(review_id)
   )`))
   .then(() => db.queryAsync(`CREATE TABLE IF NOT EXISTS Reviews_Chars (
-    id BIGSERIAL,
-    char_id INTEGER,
-    review_id INTEGER,
+    id BIGSERIAL PRIMARY KEY,
+    char_id BIGSERIAL NOT NULL,
+    review_id BIGSERIAL NOT NULL,
     value INTEGER,
     CONSTRAINT fk_review
       FOREIGN KEY(review_id)
 	      REFERENCES Reviews(review_id)
   )`))
-  .then(() => db.queryAsync(`COPY Reviews FROM '/Users/jasonmatta/Documents/HackReactorSEI/sdc/server/_data/reviews.csv' WITH (FORMAT csv, header)`))
-  .then(() => db.queryAsync(`COPY Photos FROM '/Users/jasonmatta/Documents/HackReactorSEI/sdc/server/_data/reviews_photos.csv' WITH (FORMAT csv, header)`))
-  .then(() => db.queryAsync(`COPY Reviews_Chars FROM '/Users/jasonmatta/Documents/HackReactorSEI/sdc/server/_data/characteristic_reviews.csv' WITH (FORMAT csv, header)`))
+  .then(() => db.queryAsync(`COPY Photos FROM '${process.env.REVIEWSPHOTODATA}' WITH (FORMAT csv, header)`))
+  .then(() => db.queryAsync(`COPY Reviews_Chars FROM '${process.env.REVIEWSCHARSDATA}' WITH (FORMAT csv, header)`))
