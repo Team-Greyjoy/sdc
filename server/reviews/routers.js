8e0273135b375c2e657baf6874ce923d96dc434b
Jason Matta <jt.matta@gmail.com>
2022-06-14 22:22:33 -0700

    refactor routes for loadio verification

--- server/reviews/routers.js
+++ server/reviews/routers.js
@@ -2,17 +2,17 @@ var controller = require('./controllers');
 var router = require('express').Router();
 require('dotenv').config();
 
-router.get('reviews/meta', controller.meta.get);
+router.get('/reviews/meta', controller.meta.get);
 
-router.get('reviews', controller.get);
+router.get('/reviews', controller.get);
 
-router.post('reviews', controller.post);
+router.post('/reviews', controller.post);
 
-router.put('reviews/:review_id/helpful', controller.helpful.put);
+router.put('/reviews/:review_id/helpful', controller.helpful.put);
 
-router.put('reviews/:review_id/report', controller.report.put);
+router.put('/reviews/:review_id/report', controller.report.put);
 
-router.get(`${process.env.LOADER_IO}/`, (req, res) => {
+router.get(`/${process.env.LOADER_IO}/`, (req, res) => {
   res.send(`${process.env.LOADER_IO}`)
 })
 
